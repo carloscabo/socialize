59c3b075d80f140c3ad14d3e56f857b1c8d8f6eb
carloscabo <carlos.cabo@the-cocktail.com>
2015-12-30 17:49:55 +0100

    Test using gulp-iconfot-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,28 @@
 var
   gulp     = require('gulp'),
   iconfont = require('gulp-iconfont'),
-  runTimestamp = Math.round(Date.now()/1000);
+  iconfontCss = require('gulp-iconfont-css'),
+  runTimestamp = Math.round(Date.now()/1000),
+
+  font_name = 'socialize';
 
 gulp.task('build-font', function(){
-  return gulp.src(['src/svg/*.svg'])
+  gulp.src(['src/svg/*.svg'])
+    .pipe(iconfontCss({
+      fontName: font_name,
+      path: 'src/css-template/_icons.scss',
+      targetPath: '/css/socialize/_icons.scss',
+      fontPath: '/fonts/socialize/'
+    }))
     .pipe(iconfont({
-      fontName: 'socialize', // required
+      fontName: font_name, // required
       appendUnicode: true, // recommended option
       formats: ['ttf', 'eot', 'woff'], // default, 'woff2' and 'svg' are available
       timestamp: runTimestamp, // recommended to get consistent builds when watching files
     }))
-      .on('glyphs', function(glyphs, options) {
-        // CSS templating, e.g.
-        console.log(glyphs, options);
-      })
-    .pipe(gulp.dest('fonts/'));
+    .on('glyphs', function(glyphs, options) {
+      // CSS templating, e.g.
+      console.log(glyphs, options);
+    })
+    .pipe(gulp.dest('/fonts/socialize'));
 });
