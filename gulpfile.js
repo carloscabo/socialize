33ef263c41af50283868b390c8677d835b02ce48
carloscabo <carlos.cabo@the-cocktail.com>
2015-12-30 18:05:11 +0100

    Path finetuning

--- gulpfile.js
+++ gulpfile.js
@@ -10,14 +10,14 @@ gulp.task('build-font', function(){
   gulp.src(['src/svg/*.svg'])
     .pipe(iconfontCss({
       fontName: font_name,
-      path: '/src/css-template/_icons.scss',
+      path: 'src/css-template/_socialize_styles_template.scss',
       targetPath: '../../css/socialize/_icons.scss',
       fontPath: '/fonts/socialize/'
     }))
     .pipe(iconfont({
       fontName: font_name, // required
       appendUnicode: false, // recommended option
-      formats: ['ttf', 'eot', 'woff'], // default, 'woff2' and 'svg' are available
+      formats: ['ttf', 'eot', 'woff', 'svg'], // default, 'woff2' and 'svg' are available
       timestamp: runTimestamp, // recommended to get consistent builds when watching files
     }))
     .on('glyphs', function(glyphs, options) {
