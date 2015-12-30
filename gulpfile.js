45387427328b6735156efc86583c3508ad03991c
carloscabo <carlos.cabo@the-cocktail.com>
2015-12-30 17:55:28 +0100

    Finetuning

--- gulpfile.js
+++ gulpfile.js
@@ -10,13 +10,13 @@ gulp.task('build-font', function(){
   gulp.src(['src/svg/*.svg'])
     .pipe(iconfontCss({
       fontName: font_name,
-      path: 'src/css-template/_icons.scss',
-      targetPath: '/css/socialize/_icons.scss',
+      path: '/src/css-template/_icons.scss',
+      targetPath: '../../css/socialize/_icons.scss',
       fontPath: '/fonts/socialize/'
     }))
     .pipe(iconfont({
       fontName: font_name, // required
-      appendUnicode: true, // recommended option
+      appendUnicode: false, // recommended option
       formats: ['ttf', 'eot', 'woff'], // default, 'woff2' and 'svg' are available
       timestamp: runTimestamp, // recommended to get consistent builds when watching files
     }))
@@ -24,5 +24,5 @@ gulp.task('build-font', function(){
       // CSS templating, e.g.
       console.log(glyphs, options);
     })
-    .pipe(gulp.dest('/fonts/socialize'));
+    .pipe(gulp.dest('fonts/socialize'));
 });
